c0ff9dc1ae7eb7aea99ae7cabf1610f0ae47680a
Wyro <tomasz.wyrobek@hotmail.com>
2019-02-19 20:06:40 +0100

    Fixed selecting a field in reseration table

--- Squash_reservation_system/static/selectingtable.js
+++ Squash_reservation_system/static/selectingtable.js
@@ -1,4 +1,4 @@
-$(".column100").click( function(event) {
+$("td.column2,td.column3,td.column4,td.column5,td.column6,td.column7,td.column8").click( function(event) {
            if(confirm("Are you sure you want to reserve this hour?")){
                $(this).attr("class", "gray");
            }
