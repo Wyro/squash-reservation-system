34c696dff4315152708dbb2b022b7b0c961c16be
Wyro <Wyro@4261811f904c>
2019-02-19 10:43:26 +0000

    Finally fixed the admin css issue

--- Squash_reservation_system/learning/settings.py
+++ Squash_reservation_system/learning/settings.py
@@ -21,16 +21,16 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = "/home/Wyro/squash-reservation-system/Squash_reservation_system"
+STATIC_ROOT = "/home/Wyro/squash-reservation-system/Squash_reservation_system/static"
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'tw-(yxrhz-er5yf48ip!0k&(!p#6q6%nys)w-rd%s(8t2-*88w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.wyro.pythonanywhere.com']
+ALLOWED_HOSTS = ['wyro.pythonanywhere.com']
 
 
 # Application definition
